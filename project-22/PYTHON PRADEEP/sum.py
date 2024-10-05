e2c5ff7c5660147b7724e7a524a2ddfd62f45b53
pradeepgami <pradeepgami798@gmail.com>
2024-10-05 10:37:23 +0530

    Add files via upload

---
+++ project-22/PYTHON PRADEEP/sum.py
@@ -0,0 +1,12 @@
+a = int(input("first no n = " ))
+b = int(input("second no n = " ))
+c = int(input(" cmd " ))
+
+if c==1:
+    print(a+b)
+elif c==2:
+    print(a-b)
+elif c==3:
+    print(a*b)
+elif c==4:
+    print(a/b)
